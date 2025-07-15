bd315c095e58a672df41c06cba701c74ef49f4bd
Euan Jones <joneseuan28@gmail.com>
2025-07-15 23:20:17 +0100

    Added card click event listener

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
+function createQuestion(question, answer) {
+    return {
+        question,
+        answer,
+    };
+}
 
+let testQuestion = createQuestion(
+    "This is a test question",
+    "This is a test answer"
+);
+
+console.log(testQuestion);
+
+let cardSide = document.getElementById("card-side");
+let questionText = document.getElementById("question-text");
+
+let card = document.getElementById("card");
+
+cardSide.textContent = "Question";
+questionText.textContent = testQuestion.question;
+
+card.addEventListener("click", () => {
+    questionText.textContent = testQuestion.answer
+});
