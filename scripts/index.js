0d1cc89a521815b3a8935e7b6dac36631d1c45ca
Euan Jones <joneseuan28@gmail.com>
2025-07-18 13:18:25 +0100

    Added card creation and local browser storage

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,14 @@ const cardInner = document.querySelector(".card-inner");
 const nextButton = document.getElementById("next-button");
 const prevButton = document.getElementById("prev-button");
 
+const cardCreateCont = document.querySelector(".card-creation-container");
+const cardIDText = document.getElementById("card-id");
+const addCardButton = document.getElementById("add-card-button");
+const newQuestionContent = document.getElementById("card-question");
+const newAnswerContent = document.getElementById("card-answer");
+const createCardButton = document.getElementById("create-card-button");
+const cancelCardButton = document.getElementById("cancel-card-button");
+
 function updateQuestion() {
     questionContent.textContent = questions[currentQuestion].question;
     answerContent.textContent = questions[currentQuestion].answer;
@@ -21,11 +29,17 @@ function updateQuestion() {
     totalQuestionNumber.textContent = String((questions.length));
 }
 
-function createQuestion(question, answer) {
-    return {
+function createQuestion(id, question, answer) {
+    let questionObj = {
+        id,
         question,
         answer,
     };
+
+    questions.push(questionObj);
+    localStorage.setItem(id, JSON.stringify(questionObj));
+
+    return questionObj;
 }
 
 card.addEventListener("click", () => {
@@ -42,24 +56,41 @@ prevButton.addEventListener("click", () => {
     updateQuestion();
 })
 
-questions.push(createQuestion(
+addCardButton.addEventListener("click", () => {
+    cardCreateCont.classList.toggle("isShown");
+})
+
+createCardButton.addEventListener("click", () => {
+    createQuestion(currentQuestion+1, newQuestionContent.value, newAnswerContent.value);
+    updateQuestion();
+    cardCreateCont.classList.toggle("isShown");
+})
+
+cancelCardButton.addEventListener("click", () => {
+    newQuestionContent.value = "Enter question...";
+    newAnswerContent.value = "Enter answer...";
+    cardCreateCont.classList.toggle("isShown");
+})
+
+createQuestion(
+    1,
     "What does Big O notation represent?",
-    "Algorithm efficiency or complexity.")
-);
-questions.push(createQuestion(
+    "Algorithm efficiency or complexity.");
+createQuestion(
+    2,
     "What makes HTTPS different from HTTP?",
-    "HTTPS uses encryption for secure data transfer.")
-);
-questions.push(createQuestion(
+    "HTTPS uses encryption for secure data transfer.");
+createQuestion(
+    3,
     "What is a REST API used for?",
-    "To let systems communicate over HTTP."));
-questions.push(createQuestion(
+    "To let systems communicate over HTTP.")
+createQuestion(
+    4,
     "Why is Git important in development?",
-    "It tracks code changes and supports collaboration.")
-);
-questions.push(createQuestion(
+    "It tracks code changes and supports collaboration.");
+createQuestion(
+    5,
     "What happens in a coding whiteboard interview?",
-    "You solve coding problems by writing and explaining code by hand.")
-);
+    "You solve coding problems by writing and explaining code by hand.");
 
 updateQuestion();
