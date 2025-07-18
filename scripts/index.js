3b2a7efbb70207a0bca256c8e4899b2386f76de1
Euan Jones <joneseuan28@gmail.com>
2025-07-18 01:23:23 +0100

    Implemented question progression/regression

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
+let questions = [];
+let currentQuestion = 0;
+
+const questionContent = document.getElementById('question-content');
+const answerContent = document.getElementById('answer-content');
+
+const card = document.querySelector(".card");
+const cardInner = document.querySelector(".card-inner");
+
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
+
+function updateQuestion() {
+    questionContent.textContent = questions[currentQuestion].question;
+    answerContent.textContent = questions[currentQuestion].answer;
+}
+
 function createQuestion(question, answer) {
     return {
         question,
@@ -5,14 +22,24 @@ function createQuestion(question, answer) {
     };
 }
 
-let testQuestion = createQuestion(
-    "This is a test question",
-    "This is a test answer"
-);
-
-let card = document.querySelector(".card");
-let cardInner = document.querySelector(".card-inner");
-
 card.addEventListener("click", () => {
     cardInner.classList.toggle("flipped");
 });
+
+nextButton.addEventListener("click", () => {
+   currentQuestion++;
+   updateQuestion();
+})
+
+prevButton.addEventListener("click", () => {
+    currentQuestion--;
+    updateQuestion();
+})
+
+questions.push(createQuestion("Question 1", "Answer 1"));
+questions.push(createQuestion("Question 2", "Answer 2"));
+questions.push(createQuestion("Question 3", "Answer 3"));
+questions.push(createQuestion("Question 4", "Answer 4"));
+questions.push(createQuestion("Question 5", "Answer 5"));
+
+updateQuestion();
